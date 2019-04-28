cd9affc76301fff7c2c2d5d11ed1396120059966
imengyong <1587472701@qq.com>
2019-04-28 13:59:53 +0800

    submit

---
+++ Desktop/PhotoPicker/src/main/java/me/iwf/photopicker/utils/PhotoPickerProvider.java
@@ -0,0 +1,11 @@
+package me.iwf.photopicker.utils;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by Administrator on 2018-09-14.
+ */
+
+public class PhotoPickerProvider extends FileProvider {
+
+}
